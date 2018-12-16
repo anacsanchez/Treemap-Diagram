a1b7fb34c9dceb8bcb3bfab6267d961c0923b416
Ana C Sanchez <ana.sanchez6547@gmail.com>
2018-12-16 10:29:26 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+d3.json("https://cdn.rawgit.com/freeCodeCamp/testable-projects-fcc/a80ce8f9/src/data/tree_map/video-game-sales-data.json")
+  .then(data => createTreemap(data))
+
+function createTreemap(data) {
+
+}
