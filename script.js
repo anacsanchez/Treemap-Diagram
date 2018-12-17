8390996087a4aa92ff42b3e0ddeab5ac08eddd69
Ana C Sanchez <ana.sanchez6547@gmail.com>
2018-12-16 20:33:34 -0500

    added tooltip, all tests complete

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 d3.json("https://cdn.rawgit.com/freeCodeCamp/testable-projects-fcc/a80ce8f9/src/data/tree_map/video-game-sales-data.json")
   .then(data => createTreemap(data))
 
-const width = 800;
+const width = 1000;
 const height = 800;
 const padding = 80;
 
@@ -21,7 +21,16 @@ function createTreemap(data) {
                     .append("svg")
                     .attr("width", width)
                     .attr("height", height)
+  const legend = d3.select("#treemap")
+                    .append("svg")
+                    .attr("width", width)
+                    .attr("height", height)
+                    .attr("id", "legend")
 
+  const tooltip = d3.select("body")
+                    .append("div")
+                    .attr("id", "tooltip")
+                    .style("opacity", "0")
   const root = d3.hierarchy(data)
 
   root.sum(d => d.value)
@@ -37,7 +46,14 @@ function createTreemap(data) {
                   .enter()
                   .append("g")
                   .attr("transform", d => `translate(${d.x0},${d.y0})`)
-
+                  .on("mouseover", function(d) {
+                    tooltip.style("opacity", "1")
+                            .attr("data-value", d.data.value)
+                            .html(`${d.data.value}`)
+                  })
+                  .on("mouseout", function(d) {
+                    tooltip.style("opacity", "0")
+                  })
 
   const colorScale = d3.scaleOrdinal(d3.schemeCategory10.concat(d3.schemePastel1))
                         .domain(root.children.map(d => d.data.name))
@@ -59,4 +75,21 @@ function createTreemap(data) {
       .attr("y", 20)
       .attr("width", 5)
 
+  const legendItems = legend.selectAll("g")
+        .data(root.children.map(d => d.data.name))
+        .enter()
+        .append("g")
+
+        legendItems.append("rect")
+        .attr("class", "legend-item")
+        .attr("height", 20)
+        .attr("width", 20)
+        .attr("fill", d => colorScale(d))
+        .attr("x", (d,i) => i * 50)
+        .attr("y", 0)
+        .attr("stroke", "black")
+  legendItems.append("text")
+              .attr("x", (d,i) => i * 50)
+              .attr("y", 50)
+              .text(d => d)
 }
